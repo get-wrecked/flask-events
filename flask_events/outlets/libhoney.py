5983f60dd6beb8e1e3af5e6612f3dbb9dfb3eaf6
Tarjei Husøy <git@thusoy.com>
2018-07-02 16:44:19 -0700

    Add measures/samples support for libhoney

--- flask_events/outlets/libhoney.py
+++ flask_events/outlets/libhoney.py
@@ -5,4 +5,10 @@ class LibhoneyOutlet(object):
 
 
     def handle(self, event_data, measurements, samples):
+        for key, val in measurements.items():
+            event_data['%s_seconds' % key] = val
+
+        for key, val in samples.items():
+            event_data['%s_bytes' % key] = val
+
         self.libhoney_client.send_now(event_data)
--- tests/test_libhoney_outlet.py
+++ tests/test_libhoney_outlet.py
@@ -11,3 +11,23 @@ def test_libhoney():
     client_mock.send_now.assert_called_with({
         'key': 'value',
     })
+
+
+def test_libhoney_measures():
+    client_mock = mock.Mock()
+    outlet = LibhoneyOutlet(client_mock)
+    outlet.handle({}, {'request_time': 0.123456}, {})
+
+    client_mock.send_now.assert_called_with({
+        'request_time_seconds': 0.123456,
+    })
+
+
+def test_libhoney_samples():
+    client_mock = mock.Mock()
+    outlet = LibhoneyOutlet(client_mock)
+    outlet.handle({}, {}, {'request_size': 123456})
+
+    client_mock.send_now.assert_called_with({
+        'request_size_bytes': 123456,
+    })
