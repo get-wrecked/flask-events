8128e78c0c8ed4ef91c26f875708ffaaeba3e4fd
Tarjei Husøy <git@thusoy.com>
2018-07-02 20:36:44 -0700

    Add tests for rest of sample app

--- tests/test_sample_app.py
+++ tests/test_sample_app.py
@@ -1,5 +1,6 @@
-import pytest
+import re
 
+import pytest
 from testfixtures import LogCapture
 
 from sample_app import create_app, db
@@ -11,7 +12,42 @@ def test_sample_app(sample_app_client):
 
     assert response.status_code == 200
     assert len(logs.records) == 1
-    assert 'path=/ status=200' in logs.records[0].msg
+    assert 'method=GET path=/ status=200' in logs.records[0].msg
+    assert 'database_total=' in logs.records[0].msg
+
+
+def test_sample_app_add_random(sample_app_client):
+    with LogCapture() as logs:
+        response = sample_app_client.post('/add-random')
+
+    assert response.status_code == 302
+    assert 'method=POST path=/add-random status=302' in logs.records[0].msg
+
+    assert_matches(r'database_total=0.00\ds', logs.records[0].msg)
+
+
+def test_sample_app_abort(sample_app_client):
+    with LogCapture() as logs:
+        response = sample_app_client.get('/abort')
+
+    assert response.status_code == 503
+    assert 'path=/abort status=503' in logs.records[0].msg
+    assert 'database_total=' not in logs.records[0].msg
+
+
+def test_sample_app_crash(sample_app_client):
+    with LogCapture() as logs:
+        response = sample_app_client.get('/crash')
+
+    assert response.status_code == 500
+    assert len(logs.records) == 2 # flask also logs the error
+    assert logs.records[0].name == 'flask.app'
+    assert logs.records[1].name == 'sample_app.canonical'
+    assert 'error=ValueError error_msg="no message"' in logs.records[1].msg
+
+
+def assert_matches(regex, message):
+    assert re.search(regex, message) is not None
 
 
 @pytest.fixture
