267d71d3adf357aa0fe89aae2602c993013f0636
Tarjei Husøy <git@thusoy.com>
2020-08-06 08:33:52 -0700

    Add plain py3 trove classifiers

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
